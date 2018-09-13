9592141e37a7e1d8e2d4ba986c00b34bd7cb3f04
Yashih Wu <yashih.wu@gmail.com>
2018-09-13 03:34:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(n)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
