6531b229cab8652bcb8e0557ae3e9e07789cb0a4
Yashih Wu <yashih.wu@gmail.com>
2018-09-13 03:35:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
